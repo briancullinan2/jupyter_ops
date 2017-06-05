e3a3eb3903e0e57c8b2076f91bae3537c26a041f
Brian Cullinan <megamindbrian@gmail.com>
2017-06-04 22:07:21 -0700

    no message

--- Core/index.js
+++ Core/index.js
@@ -1,19 +1,28 @@
 // How to parse a well formatted simple notebook the cheap way?
 var path = require('path');
+var Module = require('module');
+
+var newModule = new Module(__filename, process.mainModule);
+Module._cache[__filename] = newModule;
+newModule.filename = __filename;
+newModule.paths = Module._nodeModulePaths(__dirname);
 
 if(typeof importer == 'undefined') {
-    var importer = ((notebook) => {
+    ((notebook) => {
         var filename = path.basename(notebook);
         var f = fs.readFileSync(notebook).toString();
         var re = new RegExp('initialize([\\s\\S]*?)done', 'ig');
         var m; co = []; while ((m = re.exec(f)) && co.push(m[1]));
         var code = JSON.parse('["//'+co.join('","')+'"]').join('');
-        var result = eval(code + '\n\n;\n\n(imported);');
-        var importer = Object.assign({}, result[filename]);
-        return importer;
-    });
-    importer = importer(path.join(__dirname, 'How to find answers.ipynb'));
+        var footer = '\n\n;(Object.assign(module.exports, imported["'+filename+'"]))';
+        newModule._compile(code + footer, notebook);
+        newModule.loaded = true;
+    })(path.join(__dirname, 'How to find answers.ipynb'));
 }
-
-module.exports = importer;
-
+module.exports = newModule.exports;
+module.exports.interpretAll = (dirname) => Promise.all([
+    path.join(__dirname, 'How to interpret questions.ipynb'),
+    path.join(__dirname, 'How to import all notebooks.ipynb')
+    ].map(n => module.exports.importNotebook(n)))
+    .then(r => walkSync(path.join(dirname, '**', '*.ipynb'), 2))
+    .then(ns => Promise.all(ns.map(n => interpretNotebook(n))));
