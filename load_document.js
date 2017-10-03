1cef520d2cfd4e3d66634bf190507aa8cb92fa4c
SWIFTPAGE\Brian.Cullinan <megamindbrian@gmail.com>
2017-10-03 14:39:44 -0700

    no message

--- load_document.js
+++ load_document.js
@@ -3,3 +3,4 @@ document.body.appendChild(newRoot);
 newRoot.setAttribute('class', 'jupytangular');
 document.body.style.position = '';
 window.SOCKIFY_SERVER = 'https://localhost:8000';
+window.BackupPromise = window.Promise;
