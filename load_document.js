df86457b00c173f2b0b6f700c8b9f01671ce68e5
SWIFTPAGE\Brian.Cullinan <megamindbrian@gmail.com>
2017-10-03 15:31:06 -0700

    no message

--- load_document.js
+++ load_document.js
@@ -1,6 +1,15 @@
-var newRoot = document.createElement('app-root');
-document.body.appendChild(newRoot);
-newRoot.setAttribute('class', 'jupytangular');
-document.body.style.position = '';
-window.SOCKIFY_SERVER = 'https://localhost:8000';
-window.BackupPromise = window.Promise;
+document.addEventListener('DOMContentLoaded', () => {
+    const newRoot = document.createElement('app-root');
+    document.body.insertBefore(newRoot, document.body.firstChild);
+    newRoot.setAttribute('class', 'jupytangular');
+    document.body.style.position = '';
+});
+const s = document.createElement('script');
+s.setAttribute('type', 'text/javascript');
+s.innerHTML = `
+    window.BackupPromise = window.Promise;
+    window.SOCKIFY_SERVER = \'https://localhost:8000\';
+    `;
+(document.head || document.documentElement).appendChild(s);
+
+
