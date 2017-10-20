fb05e8e6952460cf14bd41152743c17e1e73af4a
Brian Cullinan <megamindbrian@gmail.com>
2017-10-19 22:52:37 -0700

    no message

--- Core/index.js
+++ Core/index.js
@@ -7,16 +7,16 @@ var newModule = new Module(__filename, process.mainModule);
 Module._cache[__filename] = newModule;
 newModule.filename = __filename;
 newModule.paths = Module._nodeModulePaths(__dirname);
-if (typeof importer == 'undefined') {
+if (typeof importer === 'undefined') {
     var importer = ((notebook, __dirname, __filename) => {
         var f = fs.readFileSync(notebook).toString();
-        var re = new RegExp('initialize([\\s\\S]*?)done', 'ig');
+        var re = new RegExp('initialize([\\s\\S]*?)\\$\\$\\.done', 'ig');
         var m, co = [];
         while ((m = re.exec(f)) && co.push(m[1])) ;
         var location = 'var __filename=' + JSON.stringify(__filename) + ';'
             + 'var __dirname=' + JSON.stringify(__dirname) + ';'
             + 'process.chdir(' + JSON.stringify(__dirname) + ');\n';
-        var code = JSON.parse('["//' + co.join('","') + '"]').join('');
+        var code = JSON.parse('["//' + co.join('","') + '"]').join('\n');
         newModule._compile(location + code, notebook);
         newModule.loaded = true;
     });
