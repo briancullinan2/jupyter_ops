32276ad583377af194216145eeab022145aab431
Brian Cullinan <megamindbrian@gmail.com>
2017-12-10 18:01:39 -0700

    selenium tools

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const functions = require('firebase-functions'),
-    admin = require('firebase-admin');
-
-try {
-    admin.initializeApp(functions.config().firebase);
-} catch (e) {
-}
-
 const importer = require('./Core');
 //for (const k of Object.keys(importer).filter(k => k.indexOf('[') === -1)) {
 //}
-
-module.exports.interpretAll = functions.https.onRequest((req, res) => {
-//    console.log(k);
-//    console.log(importer[k]);
-    console.log('interpreting ' + req.query['queries']);
-    return importer.interpretAll(req.query['queries'])
-        .then(r => res.send(JSON.stringify(r, null, 4)))
-        .catch(e => res.send(e + ''));
-});
+module.exports = importer.import('firebase rpc wrapper');
