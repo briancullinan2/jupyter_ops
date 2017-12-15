d0710f7b5b939b9764e71a760375cfd64c95dfed
Brian Cullinan <megamindbrian@gmail.com>
2017-12-15 09:44:23 -0700

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+    target: 'node',
+    entry: './index.js',
+    output: {
+        filename: './bundle.js'
+    },
+    plugins: [
+//    new webpack.BannerPlugin('require("source-map-support").install();',
+//                                { raw: true, entryOnly: false })
+    ],
+    node: {
+    },
+    externals: nodeModules
+}
