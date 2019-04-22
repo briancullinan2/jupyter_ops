1c81499192646a46a9f5f1f7fea126bc75a93e87
Brian Cullinan <megamindbrian@gmail.com>
2019-04-22 15:57:02 -0700

    "auto-commit"

--- .functions/index.js
+++ .functions/index.js
@@ -8,5 +8,11 @@
 var getSheet = require('./get-sheet-identifier-from-link.js');
 
 exports.handler = async (req, res) => {
-  return await getSheet(req.query['link']);
+    res.set('Access-Control-Allow-Origin', '*');
+    return await getSheet(req.query['link'])
+        .then(r => res.status(200).send(r))
+        .catch(e => res.status(500).send(Object.getOwnPropertyNames(e).reduce((alt, key) => {
+            alt[key] = e[key] + '';
+            return alt;
+        }, {})));
 };
