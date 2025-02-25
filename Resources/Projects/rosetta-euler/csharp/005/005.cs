c249ba66878a39e3c0cd76bb4b45317fb0797cbc
Brian Cullinan <megamindbrian@gmail.com>
2025-02-25 09:36:57 -0700

    node-api-dotnet works but edge-js does not.

--- Resources/Projects/rosetta-euler/csharp/005/005.cs
+++ Resources/Projects/rosetta-euler/csharp/005/005.cs
@@ -1,59 +1,83 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
-public class Euler {
-    public void Main(object[] args) {
-        List<List<ulong>> primeFactorLists = new List<List<ulong>>();
+namespace Rosetta
+{
 
-        for (ulong x = 1; x <= 20; x++) {
-            primeFactorLists.Add(PrimeFactors(x));
+    public class Euler
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", new Euler().Calculate());
         }
 
-        List<ulong> factors = new List<ulong>();
+        public ulong Calculate()
+        {
+            List<List<ulong>> primeFactorLists = new List<List<ulong>>();
 
-        for (int i = 0; i < 20; i++) {
-            factors = Overlap(factors, primeFactorLists[i]);
-        }
-
-        Console.WriteLine("{0}", factors.Aggregate((product, factor) => product * factor));
-    }
-
-    public List<ulong> Overlap(List<ulong> a, List<ulong> b) {
-        for (int i = 0; i < b.Count; i++) {
-            a.Remove(b[i]);
-        }
-
-        a.AddRange(b);
-
-        return a;
-    }
-
-    public List<ulong> PrimeFactors(ulong n) {
-        bool found = false;
-
-        ulong v = 2;
-
-        for (ulong i = 2; i < n - 1; i++) {
-            if (n % i == 0) {
-                found = true;
-
-                v = i;
-
-                break;
+            for (ulong x = 1; x <= 20; x++)
+            {
+                primeFactorLists.Add(PrimeFactors(x));
             }
+
+            List<ulong> factors = new List<ulong>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                factors = Overlap(factors, primeFactorLists[i]);
+            }
+
+            return factors.Aggregate((product, factor) => product * factor);
         }
 
-        List<ulong> result = new List<ulong>();
+        public List<ulong> Overlap(List<ulong> a, List<ulong> b)
+        {
+            for (int i = 0; i < b.Count; i++)
+            {
+                a.Remove(b[i]);
+            }
 
-        if (found) {
-            result.Add(v);
+            a.AddRange(b);
 
-            result = result.Concat(PrimeFactors(n / v)).ToList();
-        } else {
-            result.Add(n);
+            return a;
         }
 
-        return result;
+        public List<ulong> PrimeFactors(ulong n)
+        {
+            bool found = false;
+
+            ulong v = 2;
+
+            for (ulong i = 2; i < n - 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    found = true;
+
+                    v = i;
+
+                    break;
+                }
+            }
+
+            List<ulong> result = new List<ulong>();
+
+            if (found)
+            {
+                result.Add(v);
+
+                result = result.Concat(PrimeFactors(n / v)).ToList();
+            }
+            else
+            {
+                result.Add(n);
+            }
+
+            return result;
+        }
     }
+
 }
+
