6b4a9e6bf44fa4d2e24ee96f215001656ec8c0d7
Brian Cullinan <megamindbrian@gmail.com>
2025-04-14 14:18:31 -0700

    file system reorganizing script

---
+++ .automations/2025-04-14T20_39_39_450Z-download_reorganize_bash.sh
@@ -0,0 +1,66 @@
+cd /home/Downloads;
+mv "a_beautiful_woman_goes_to_vote_for_the_polish_president_and_walks_through_a_portal_and_replaced_by_a.html (1).pdf" "./PDFs/" || :
+mv "a_beautiful_woman_goes_to_vote_for_the_polish_president_and_walks_through_a_portal_and_replaced_by_a.html (2).pdf" "./PDFs/" || :
+mv "a_beautiful_woman_goes_to_vote_for_the_polish_president_and_walks_through_a_portal_and_replaced_by_a.html.pdf" "./PDFs/" || :
+mv "a_duck_swimming_in_a_pond_for_the_first_time.html (1).pdf" "./PDFs/" || :
+mv "a_duck_swimming_in_a_pond_for_the_first_time.html.pdf" "./PDFs/" || :
+mv "ai_and_sex_robots_will_only_valorise_the_cost_of_real_humans.html (1).pdf" "./PDFs/" || :
+mv "ai_and_sex_robots_will_only_valorise_the_cost_of_real_humans.html (2).pdf" "./PDFs/" || :
+mv "ai_and_sex_robots_will_only_valorise_the_cost_of_real_humans.html (3).pdf" "./PDFs/" || :
+mv "ai_and_sex_robots_will_only_valorise_the_cost_of_real_humans.html.pdf" "./PDFs/" || :
+mv "an_organic_all_natural_cane_sugar_lemonade_stand_2.html.pdf" "./PDFs/" || :
+mv "an_organic_all_natural_cane_sugar_lemonade_stand_3.html.pdf" "./PDFs/" || :
+mv "an_organic_all_natural_cane_sugar_lemonade_stand.html.pdf" "./PDFs/" || :
+mv "assisted_living_elderly_care_service.html.pdf" "./PDFs/" || :
+mv "automatic_car_wash.html.pdf" "./PDFs/" || :
+mv "answer_page_xlsx_1Uo6MJftZdNL2V5cLq9bmcnjAdPEgBjXa2ztIhshQxdo.xlsx" "./Data Analytics/" || :
+mv "active directory.ipynb" "./Jupyter Tools/" || :
+mv "ad_sepulcher.zip" "./Game Projects/" || :
+mv "aearena.zip" "./Game Projects/" || :
+mv "akutatourney5.zip" "./Game Projects/" || :
+mv "amph-dm01 (1).zip" "./Game Projects/" || :
+mv "amph-dm01 (1)" "./Game Projects/" || :
+mv "amph-dm01.zip" "./Game Projects/" || :
+mv "an_apple.obj" "./3D/" || :
+mv "auth_mdl-r26 (1).zip" "./Auth Security/" || :
+mv "auth_mdl-r26.zip" "./Auth Security/" || :
+mv "aw_beta1 (1).zip" "./Game Projects/" || :
+mv "aw_beta1.zip" "./Game Projects/" || :
+mv "aw_beta2 (1).zip" "./Game Projects/" || :
+mv "aw_beta2 (2).zip" "./Game Projects/" || :
+mv "aw_beta2.zip" "./Game Projects/" || :
+mv "aw_code (1).zip" "./Game Projects/" || :
+mv "aw_code (2).zip" "./Game Projects/" || :
+mv "aw_code.zip" "./Game Projects/" || :
+mv "axdm1.zip" "./Game Projects/" || :
+mv "axdm1" "./Game Projects/" || :
+mv "bal3void2.zip" "./Game Projects/" || :
+mv "bal3void2" "./Game Projects/" || :
+mv "bank_fsb.rar" "./Misc Projects/" || :
+mv "base.dmg" "./Installers/" || :
+mv "baseq3a-master.zip" "./Game Projects/" || :
+mv "baseq3a-NeoHud-master.zip" "./Game Projects/" || :
+mv "bashcode.zip" "./Shell Scripts/" || :
+mv "beneath (1).zip" "./Game Projects/" || :
+mv "beneath.zip" "./Game Projects/" || :
+mv "bery129src.zip" "./Game Projects/" || :
+mv "bg_physics.c" "./Game Projects/" || :
+mv "bg_physics.h" "./Game Projects/" || :
+mv "bg_wheel_forces.c" "./Game Projects/" || :
+mv "bidforpower1-2 (1).exe" "./Installers/" || :
+mv "bidforpower1-2.exe" "./Installers/" || :
+mv "bidforpower1-2" "./Installers/" || :
+mv "binaryen-version_116-arm64-macos.tar.gz" "./Emscripten/" || :
+mv "binaryen-version_118-x86_64-linux.tar.gz" "./Emscripten/" || :
+mv "blender_ie_quake_mdl-master.zip" "./Game Projects/" || :
+mv "blender-4.2.0-macos-arm64.dmg" "./Installers/" || :
+mv "blender-md3-2.8-blender2.8.zip" "./Game Projects/" || :
+mv "blender-md3-master (1).zip" "./Game Projects/" || :
+mv "blender-md3-master (2).zip" "./Game Projects/" || :
+mv "blender-md3-master.zip" "./Game Projects/" || :
+mv "bloodrun-2024-06-29 (1).zip" "./Game Projects/" || :
+mv "bloodrun-2024-06-29 (2).zip" "./Game Projects/" || :
+mv "bloodrun-2024-06-29.zip" "./Game Projects/" || :
+mv "bloodrun-2024-06-29" "./Game Projects/" || :
+mv "about" "./Docs Guides/" || :
+mv "aearena" "./Game Projects/" || :
---
+++ .automations/2025-04-14T21_10_09_940Z-download_reorganize_bash.sh
@@ -0,0 +1,64 @@
+cd /home/Downloads;
+mv "megamindbrian@gmail.com.ical (68).zip" "./Google/" || :
+mv "megamindbrian@gmail.com.ical (7).zip" "./Google/" || :
+mv "megamindbrian@gmail.com.ical (8).zip" "./Google/" || :
+mv "megamindbrian@gmail.com.ical (9).zip" "./Google/" || :
+mv "megamindbrian@gmail.com.ical.zip" "./Google/" || :
+mv "megatron_robot_form.webp" "./Images/" || :
+mv "messages_0 (1).3gpp" "./Media/" || :
+mv "messages_0 (2).3gpp" "./Media/" || :
+mv "messages_0.3gpp" "./Media/" || :
+mv "metal_bonding (1).html" "./Docs/" || :
+mv "metal_bonding.html" "./Docs/" || :
+mv "micropython.wasi" "./Misc/" || :
+mv "minetest-5.8.0-macos.zip" "./Engines/" || :
+mv "mint-metal-master.zip" "./Assets/" || :
+mv "mjolnir1.0.zip" "./Games/" || :
+mv "mm3d-1_2_4-win32-installer.exe" "./Installers/" || :
+mv "mm3d-1_3_14-win32-installer.exe" "./Installers/" || :
+mv "mm3d-1_3_8-win32-installer.exe" "./Installers/" || :
+mv "mm3d-1.2.4.tar.gz" "./Code/" || :
+mv "mm3d-1.3.14.dmg" "./Installers/" || :
+mv "mm3d-1.3.8a.tar.gz" "./Code/" || :
+mv "mm3d-master (1).zip" "./Games/" || :
+mv "mm3d-master.zip" "./Games/" || :
+mv "model.safetensors" "./AI/" || :
+mv "modelconv" "./Misc/" || :
+mv "modelconv-master.zip" "./Code/" || :
+mv "modelconv.rar" "./Misc/" || :
+mv "models 2" "./Misc/" || :
+mv "models-20240719T150421Z-001.zip" "./AI/" || :
+mv "models.zip" "./AI/" || :
+mv "moe.clover.mm3d-1.3.14.flatpak" "./Installers/" || :
+mv "monkeysofdoomV01.zip" "./Games/" || :
+mv "moteof_final2 (1)" "./Misc/" || :
+mv "moteof_final2 (1).zip" "./Assets/" || :
+mv "moteof_final2.zip" "./Assets/" || :
+mv "mountain_Day_Spa_and_Hair_Nail_Salon.html.pdf" "./PDFs/" || :
+mv "mountain-trees" "./Misc/" || :
+mv "mountain-trees.zip" "./Assets/" || :
+mv "ms3d184 (1).zip" "./Games/" || :
+mv "ms3d184 (2).zip" "./Games/" || :
+mv "ms3d184.zip" "./Games/" || :
+mv "ms3d185beta1 (1).zip" "./Games/" || :
+mv "ms3d185beta1.zip" "./Games/" || :
+mv "ms3d185setup.exe" "./Installers/" || :
+mv "ms3dsdk184.zip" "./Code/" || :
+mv "my_wife_is_a_blood_sucking_vampire__she_goes_to_market_to_feed_and_says_shes_getting_food_but_comes_.html (1) (1).pdf" "./PDFs/" || :
+mv "my_wife_is_a_blood_sucking_vampire__she_goes_to_market_to_feed_and_says_shes_getting_food_but_comes_.html (1) (2).pdf" "./PDFs/" || :
+mv "my_wife_is_a_blood_sucking_vampire__she_goes_to_market_to_feed_and_says_shes_getting_food_but_comes_.html (1) (3).pdf" "./PDFs/" || :
+mv "my_wife_is_a_blood_sucking_vampire__she_goes_to_market_to_feed_and_says_shes_getting_food_but_comes_.html (1).pdf" "./PDFs/" || :
+mv "my_wife_is_a_blood_sucking_vampire__she_goes_to_market_to_feed_and_says_shes_getting_food_but_comes_.html (2).pdf" "./PDFs/" || :
+mv "my_wife_is_a_blood_sucking_vampire__she_goes_to_market_to_feed_and_says_shes_getting_food_but_comes_.html.pdf" "./PDFs/" || :
+mv "my_wife_is_an_angel__she_gives_kids_baths_and_does_homework_with_them_and_gets_yelled_at_by_the_neig.html.pdf" "./PDFs/" || :
+mv "name.basics.tsv" "./Data/" || :
+mv "name.basics.tsv.gz" "./Data/" || :
+mv "netradiant_1.5.0-20220628-macos-amd64" "./Engines/" || :
+mv "netradiant_1.5.0-20220628-macos-amd64.tar.xz" "./Code/" || :
+mv "nikolay-sokolov-orbb.jpg" "./Images/" || :
+mv "nodm10" "./Misc/" || :
+mv "nodm10.zip" "./Games/" || :
+mv "nodm13" "./Misc/" || :
+mv "nodm13.zip" "./Games/" || :
+mv "noesisv4474.zip" "./Engines/" || :
+mv "nor3ctf1" "./Misc/" || :
